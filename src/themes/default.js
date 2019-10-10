3bb23aabbdaf7e2824eebb15bc68b5e34c35c1e1
Jonas Munsch <jonas1.munsch@web.de>
2019-10-10 16:33:09 +0200

    define defaultTheme

---
+++ src/themes/default.js
@@ -0,0 +1,11 @@
+const defaultTheme = {
+  main: "#22262F",
+  lightFont: "#fff",
+  darkFont: "#9F9898",
+  highlight: "#6F7477",
+  lightBackground: "#707070",
+  accent: "#404345",
+  brightEffect: "#F0A42A"
+};
+
+export default defaultTheme;
