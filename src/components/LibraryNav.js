a3d7d1ca84b832cf62ae147c097c8fa3c30af736
Jonas Munsch <jonas1.munsch@web.de>
2019-10-11 16:46:58 +0200

    add library navbar

---
+++ src/components/LibraryNav.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavGrid = styled.div`
+  display: grid;
+  height: 75px;
+  grid-template-columns: 1fr 1fr 1fr;
+  border: none;
+`;
+
+const NavGridButton = styled.button`
+  color: ${props => (props ? "white" : props.theme.darkFont)};
+  background: ${props => props.theme.accent};
+  font-size: 22px;
+  display: flex;
+  width: 100%;
+  height: inherit;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  border-bottom: ${props =>
+    props.selected ? `8px solid ${props.theme.brightEffect}` : "none"};
+  color: ${props => (props.selected ? "white" : props.theme.darkFont)};
+`;
+
+export default function LibraryNav() {
+  return (
+    <NavGrid>
+      <NavGridButton selected>Collection</NavGridButton>
+      <NavGridButton>Browse</NavGridButton>
+      <NavGridButton>Wishlist</NavGridButton>
+    </NavGrid>
+  );
+}
---
+++ src/stories/LibraryNav.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LibraryNav from "../components/LibraryNav";
+
+export default {
+  title: "LibraryNav"
+};
+
+export const LibraryNavbar = () => {
+  return <LibraryNav />;
+};
