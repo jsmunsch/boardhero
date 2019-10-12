077bc77ae5f326c5fceca91ec40d0e0c3acf1489
Jonas Munsch <jonas1.munsch@web.de>
2019-10-12 15:59:24 +0200

    remove dead code

--- src/stories/DetailCard.stories.js
+++ src/stories/DetailCard.stories.js
@@ -1,22 +1,10 @@
 import React from "react";
-import CardImage from "../components/CardModal";
-import CardBadge from "../components/CardBadge";
 import { GamePositioned } from "../components/Game";
-import AbsoluteImageContainer from "../components/Game";
 import CardModal from "../components/CardModal";
 
 export default {
   title: "DetailCard"
 };
-
-// export const CardLayout = () => <CardModal />;
-
-// export const CardWithBadges = () => (
-//   <CardModal>
-//     <CardBadge>Hallo</CardBadge>
-//     <CardBadge>Test</CardBadge>
-//   </CardModal>
-// );
 
 export const CardWithImage = () => (
   <CardModal>
