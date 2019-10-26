c33e753dce14d94eb5157d852cf1f71710a35105
Jonas Munsch <jonas1.munsch@web.de>
2019-10-26 19:33:45 +0200

    configure correct build json

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 initDatabase(process.env.DB_URL, process.env.DB_NAME).then(() => {
