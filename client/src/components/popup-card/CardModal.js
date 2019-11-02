6d653c6474be772256872c7c3a91348c3c98a3c5
jsmunsch <54038089+jsmunsch@users.noreply.github.com>
2019-11-02 12:17:16 -0700

    create own folder for library components (#28)

--- client/src/components/popup-card/CardModal.js
+++ client/src/components/popup-card/CardModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import CollectionItemPositioned from "../GamePositioned";
+import CollectionItemPositioned from "./GamePositioned";
 import CardBadge from "./CardBadge";
 import CardDetails from "./CardDetails";
 import CardCategories from "./CardCategories";
