3b2e4ceed7ab15b1520ea9279c13ed62a1c4ded3
Jonas Munsch <jonas1.munsch@web.de>
2019-11-07 22:46:52 +0100

    add placeholder to recently added page while no game have been added

--- client/src/pages/UserOverview.js
+++ client/src/pages/UserOverview.js
@@ -2,24 +2,39 @@ import React from "react";
 import MainContent from "../components/User-page.js/MainContent";
 import Title from "../components/User-page.js/Title";
 import RecentlyAdded from "../components/User-page.js/RecentlyAdded";
+import Placeholder from "../components/User-page.js/Placeholder";
+import { useHistory } from "react-router-dom";
 
-export default function UserOverview({ filteredGames }) {
+export default function UserOverview({
+  filteredGames,
+  text,
+  buttonText,
+  onClick
+}) {
+  let history = useHistory();
+  let bool = filteredGames.name;
   return (
     <MainContent>
-      <Title>
-        <span>Recently Added</span>
-      </Title>
-      {filteredGames &&
-        filteredGames
-          .slice(0, 5)
-          .map(game => (
-            <RecentlyAdded
-              key={game.id}
-              src={game.image_url}
-              name={game.name}
-              description={game.id}
-            />
-          ))}
+      {!bool && (
+        <Placeholder
+          text="Is this your first time here ? To display your recently added games add some first. Let's go!"
+          buttonText="Add Games"
+          onClick={() => history.push("/library/browse")}
+        />
+      )}
+      {bool && (
+        <Title>
+          <span>Recently Added</span>
+        </Title>
+      )}
+      {filteredGames.slice(0, 5).map(game => (
+        <RecentlyAdded
+          key={game.id}
+          src={game.image_url}
+          name={game.name}
+          description={game.id}
+        />
+      ))}
     </MainContent>
   );
 }
